e9f6e29fadfea681a05553ab92c0523953127539
Jason Zhu <junjie.zhu.jason@gmail.com>
2019-10-18 15:40:01 -0700

    added entero fit

--- src/main_methods.py
+++ src/main_methods.py
@@ -288,52 +288,52 @@ def compute_all_embeddings(result, c_mtx, x):
             embed_dict[key] = df
     return embed_dict
 
-# def optimalK(data, nrefs=3, maxClusters=20):
-#     """
-#     Calculates KMeans optimal K using Gap Statistic from Tibshirani, Walther, Hastie
-#     Params:
-#         data: ndarry of shape (n_samples, n_features)
-#         nrefs: number of sample reference datasets to create
-#         maxClusters: Maximum number of clusters to test for
-#     Returns: (gaps, optimalK)
+def optimalK(data, nrefs=3, maxClusters=20):
+    """
+    Calculates KMeans optimal K using Gap Statistic from Tibshirani, Walther, Hastie
+    Params:
+        data: ndarry of shape (n_samples, n_features)
+        nrefs: number of sample reference datasets to create
+        maxClusters: Maximum number of clusters to test for
+    Returns: (gaps, optimalK)
     
-#     # Source: https://anaconda.org/milesgranger/gap-statistic/notebook
-#     """
-#     gaps = np.zeros((len(range(1, maxClusters)),))
-#     resultsdf = pd.DataFrame({'clusterCount':[], 'gap':[]})
-#     for gap_index, k in enumerate(range(1, maxClusters)):
+    # Source: https://anaconda.org/milesgranger/gap-statistic/notebook
+    """
+    gaps = np.zeros((len(range(1, maxClusters)),))
+    resultsdf = pd.DataFrame({'clusterCount':[], 'gap':[]})
+    for gap_index, k in enumerate(range(1, maxClusters)):
 
-#         # Holder for reference dispersion results
-#         refDisps = np.zeros(nrefs)
+        # Holder for reference dispersion results
+        refDisps = np.zeros(nrefs)
 
-#         # For n references, generate random sample and perform kmeans getting resulting dispersion of each loop
-#         for i in range(nrefs):
+        # For n references, generate random sample and perform kmeans getting resulting dispersion of each loop
+        for i in range(nrefs):
             
-#             # Create new random reference set
-#             randomReference = np.random.random_sample(size=data.shape)
+            # Create new random reference set
+            randomReference = np.random.random_sample(size=data.shape)
             
-#             # Fit to it
-#             km = KMeans(k)
-#             km.fit(randomReference)
+            # Fit to it
+            km = KMeans(k)
+            km.fit(randomReference)
             
-#             refDisp = km.inertia_
-#             refDisps[i] = refDisp
+            refDisp = km.inertia_
+            refDisps[i] = refDisp
 
-#         # Fit cluster to original data and create dispersion
-#         km = KMeans(k)
-#         km.fit(data)
+        # Fit cluster to original data and create dispersion
+        km = KMeans(k)
+        km.fit(data)
         
-#         origDisp = km.inertia_
+        origDisp = km.inertia_
 
-#         # Calculate gap statistic
-#         gap = np.log(np.mean(refDisps)) - np.log(origDisp)
+        # Calculate gap statistic
+        gap = np.log(np.mean(refDisps)) - np.log(origDisp)
 
-#         # Assign this loop's gap statistic to gaps
-#         gaps[gap_index] = gap
+        # Assign this loop's gap statistic to gaps
+        gaps[gap_index] = gap
         
-#         resultsdf = resultsdf.append({'clusterCount':k, 'gap':gap}, ignore_index=True)
+        resultsdf = resultsdf.append({'clusterCount':k, 'gap':gap}, ignore_index=True)
 
-#     return (gaps.argmax() + 1, resultsdf)  # Plus 1 because index of 0 means 1 cluster is optimal, index 2 = 3 clusters are optimal
+    return (gaps.argmax() + 1, resultsdf)  # Plus 1 because index of 0 means 1 cluster is optimal, index 2 = 3 clusters are optimal
 
 
 # def graph_eig_decomp(A, plot = True, topK = 5, max_clust=10):
